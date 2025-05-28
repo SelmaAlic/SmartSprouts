64ccdfd15a7e39df9918d0e30c9c1797a3826a21
AdnaKikanovic505 <kikanovicadna@gmail.com>
2025-05-28 18:14:07 +0200

    Moving files from GUI-login to main

---
+++ auth.py
@@ -0,0 +1,15 @@
+import sqlite3
+from encryption import verify_encryption
+
+def authenticate(username, raw_password):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT password FROM login_info WHERE username = ?", (username,)) #gets the password from database
+    result = cursor.fetchone()
+    conn.close()
+
+    if result:
+        stored_encrypted_password = result[0]
+        return verify_encryption(raw_password, stored_encrypted_password)
+    return False
