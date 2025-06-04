f383febb1cad15cb8df08f1f619395c877c8628d
Muhamed Hodžić <muhamed.hodzic.h@gmail.com>
2025-06-04 13:05:14 +0200

    Delete encryption.py

--- encryption.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-
-def to_encrypt(password):
-    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-    return hashed.decode() #this will be stored in database
-
-def verify_encryption(input_password, stored_hashed_password):
-    return bcrypt.checkpw(input_password.encode(), stored_hashed_password.encode()) #checks raw password with password from database
